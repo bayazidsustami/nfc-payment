139ebb1664487cbf690d1c7601833baaa58e6191
bayazidsustami <bayazidsustamy@gmail.com>
2024-05-13 22:58:23 +0700

    feat : implement to activity with payload

--- app/src/main/java/com/example/nfcpayment/MainActivity.kt
+++ app/src/main/java/com/example/nfcpayment/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.nfcpayment
 
+import android.content.Context
+import android.content.Intent
+import android.nfc.NfcManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val etInput = findViewById<AppCompatEditText>(R.id.et_msg)
+        val btnEmulate = findViewById<AppCompatButton>(R.id.btn_emulate)
+
+        btnEmulate.setOnClickListener {
+            val inputtedText = etInput.text.toString()
+
+            if (inputtedText.isEmpty()) {
+                showToast("payload can't be empty")
+                return@setOnClickListener
+            }
+
+            if (isNfcAvailable()) {
+                val serviceIntent = Intent(this, MyHostApduService::class.java).also {
+                    it.putExtra(MyHostApduService.PAYLOAD_EXTRAS, inputtedText)
+                }
+                startService(serviceIntent)
+            } else {
+                showToast("nfc not available or not active please check it")
+            }
         }
     }
+
+    private fun isNfcAvailable() : Boolean {
+        val nfcManager = getSystemService(Context.NFC_SERVICE) as NfcManager
+        val nfcAdapter = nfcManager.defaultAdapter
+        return nfcAdapter != null && nfcAdapter.isEnabled
+    }
+
 }
