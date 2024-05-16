2c7369fb0dbdb81db51e21b93d760215557368d1
bayazidsustami <bayazidsustamy@gmail.com>
2024-05-16 12:31:15 +0700

    chore : change apdu service

--- app/src/main/java/com/example/nfcpayment/MyHostApduService.kt
+++ app/src/main/java/com/example/nfcpayment/MyHostApduService.kt
@@ -14,111 +14,29 @@ class MyHostApduService: HostApduService() {
     companion object {
         const val PAYLOAD_EXTRAS = "payload-extras"
 
-        //
-        // We use the default AID from the HCE Android documentation
-        // https://developer.android.com/guide/topics/connectivity/nfc/hce.html
-        //
-        // Ala... <aid-filter android:name="F0394148148100" />
-        //
-        private val APDU_SELECT = byteArrayOf(
-            0x00.toByte(),
-            0xA4.toByte(),
-            0x04.toByte(),
-            0x00.toByte(),
-            0x07.toByte(),
-            0xF0.toByte(),
-            0x39.toByte(),
-            0x41.toByte(),
-            0x48.toByte(),
-            0x14.toByte(),
-            0x81.toByte(),
-            0x00.toByte(),
-            0x00.toByte(),
-        )
-
-        private val CAPABILITY_CONTAINER = byteArrayOf(
-            0x00.toByte(),
-            0xa4.toByte(),
-            0x00.toByte(),
-            0x0c.toByte(),
-            0x02.toByte(),
-            0xe1.toByte(),
-            0x03.toByte(),
-        )
-
-        private val READ_CAPABILITY_CONTAINER = byteArrayOf(
-            0x00.toByte(),
-            0xb0.toByte(),
-            0x00.toByte(),
-            0x00.toByte(),
-            0x0f.toByte(),
-        )
-
-        private val READ_CAPABILITY_CONTAINER_RESPONSE = byteArrayOf(
-            0x00.toByte(),
-            0x0F.toByte(),
-            0x20.toByte(),
-            0x00.toByte(),
-            0x3B.toByte(),
-            0x00.toByte(),
-            0x34.toByte(),
-            0x04.toByte(),
-            0x06.toByte(),
-            0xE1.toByte(),
-            0x04.toByte(),
-            0x00.toByte(),
-            0x32.toByte(),
-            0x00.toByte(),
-            0x00.toByte(),
-            0x90.toByte(),
-            0x00.toByte()
-        )
-
-        private val NDEF_SELECT = byteArrayOf(
-            0x00.toByte(),
-            0xa4.toByte(),
-            0x00.toByte(),
-            0x0c.toByte(),
-            0x02.toByte(),
-            0xE1.toByte(),
-            0x04.toByte()
-        )
-
-        private val NDEF_READ_BINARY_NLEN = byteArrayOf(
-            0x00.toByte(),
-            0xb0.toByte(),
-            0x00.toByte(),
-            0x00.toByte(),
-            0x02.toByte(),
-        )
-
-        private val NDEF_READ_BINARY_GET_NDEF = byteArrayOf(
-            0x00.toByte(),
-            0xb0.toByte(),
-            0x00.toByte(),
-            0x00.toByte(),
-            0x0f.toByte(),
-        )
-
-        private val A_OKAY = byteArrayOf(
-            0x90.toByte(),
-            0x00.toByte(),
-        )
-
         private val NDEF_ID = byteArrayOf(
             0xE1.toByte(),
             0x04.toByte(),
         )
 
-    }
+        const val STATUS_SUCCESS = "9000"
+        const val STATUS_FAILED = "0000"
+        const val CLA_NOT_SUPPORTED = "6E00"
+        const val INS_NOT_SUPPORTED = "6D00"
+        const val AID = "A0000006022020"
+        const val SELECT_INS = "A4"
+        const val DEFAULT_CLA = "00"
+        const val MIN_APDU_LENGTH = 12
+        const val SEND_PAYLOAD = "00B20100"
+        const val SEND_PAYLOAD1 = "00B20200"
 
-    private var isCapableReadContainer = false
+    }
 
     private var ndefUri = NdefRecord(
         NdefRecord.TNF_WELL_KNOWN,
         NdefRecord.RTD_TEXT,
         NDEF_ID,
-        "Hello world!".toByteArray(),
+        "hQVDUFYwMWFcTwegAAAGAiAgUAdRUklTQ1BNWgqTYAACISAJl0KPXyAOcml6a2EgcmFtYWRoYW5fLQRpZGVuX1AVYWRob24ucml6a3lAZ21haWwuY29tnyUCl0JjB590BGFlNjE=".toByteArray(),
     )
 
     private var ndefUriByte = ndefUri.toByteArray()
@@ -146,81 +64,38 @@ class MyHostApduService: HostApduService() {
         return Service.START_STICKY_COMPATIBILITY
     }
 
-    override fun processCommandApdu(p0: ByteArray, p1: Bundle): ByteArray {
-        log("processCommandApdu() | incoming commandApdu: " + Utils.bytesToHex(p0))
+    override fun processCommandApdu(commandApdu: ByteArray?, p1: Bundle?): ByteArray {
+        val payload = "test test test test test tset test test test test test test test test "
+        val payload2 = "test test test test test tes test test"
 
-        /**
-         * First command: NDEF Tag Application select (Section 5.5.2 in NFC Forum spec)
-         */
-        if (Utils.isEqual(APDU_SELECT, p0)) {
-            log("APDU_SELECT triggered. Our Response: " + Utils.bytesToHex(A_OKAY))
-            return A_OKAY
+        val payloadByteArray = payload.toByteArray()
+        val paylaodByteArray2 = payload2.toByteArray()
+
+        if (commandApdu == null) {
+            log("null")
+            return Utils.hexStringToByteArray(STATUS_FAILED)
         }
 
-        /**
-         * Second command: Capability Container select (Section 5.5.3 in NFC Forum spec)
-         */
-        if (Utils.isEqual(CAPABILITY_CONTAINER, p0)) {
-            log("CAPABILITY_CONTAINER triggered. Our Response: " + Utils.bytesToHex(A_OKAY))
-            return A_OKAY
+        val hexCommandApdu = Utils.bytesToHex(commandApdu)
+        log("command apdu : $hexCommandApdu")
+        if (hexCommandApdu.length < MIN_APDU_LENGTH) {
+            log("min apdu length")
+            return Utils.hexStringToByteArray(STATUS_FAILED)
         }
 
-        /**
-         * Third command: ReadBinary data from CC file (Section 5.5.4 in NFC Forum spec)
-         */
-        if (Utils.isEqual(READ_CAPABILITY_CONTAINER, p0) && !isCapableReadContainer) {
-            log("READ_CAPABILITY_CONTAINER triggered. Our Response: " + Utils.bytesToHex(READ_CAPABILITY_CONTAINER_RESPONSE))
-            isCapableReadContainer = true
-            return READ_CAPABILITY_CONTAINER_RESPONSE
+        if (hexCommandApdu == "00A4040007A0000006022020")  {
+            return Utils.hexStringToByteArray(STATUS_SUCCESS)
         }
 
-
-        /**
-         * Fourth command: NDEF Select command (Section 5.5.5 in NFC Forum spec)
-         */
-        if (Utils.isEqual(NDEF_SELECT, p0)) {
-            log("NDEF_SELECT triggered. Our Response: " + Utils.bytesToHex(A_OKAY))
-            return A_OKAY
+        if (hexCommandApdu == "00B2010007A0000006022020") {
+            return payloadByteArray + Utils.hexStringToByteArray("61FF")
         }
 
-        /**
-         * Fifth command:  ReadBinary, read NLEN field
-         */
-        if (Utils.isEqual(NDEF_READ_BINARY_NLEN, p0)) {
-            val start = byteArrayOf(0x00.toByte())
-            val response = ByteArray(start.size + ndefUriLen.size + A_OKAY.size)
-            System.arraycopy(start, 0, response, 0, start.size)
-            System.arraycopy(ndefUriLen, 0, response, start.size, ndefUriLen.size)
-            System.arraycopy(A_OKAY, 0, response, start.size + ndefUriLen.size, A_OKAY.size)
-
-            log(response.toString())
-            log("NDEF_READ_BINARY_NLEN triggered. Our Response: " + Utils.bytesToHex(response))
-            return response
+        return if (hexCommandApdu == "00B2020007A0000006022020") {
+            paylaodByteArray2 + Utils.hexStringToByteArray(STATUS_SUCCESS)
+        } else {
+            Utils.hexStringToByteArray(STATUS_FAILED)
         }
-
-        /**
-         * Sixth command: ReadBinary, get NDEF data
-         */
-        if (Utils.isEqual(NDEF_READ_BINARY_GET_NDEF, p0)) {
-            val start = byteArrayOf(0x00.toByte())
-            val response = ByteArray(start.size + ndefUriLen.size + ndefUriByte.size + A_OKAY.size)
-
-            System.arraycopy(start, 0, response, 0, start.size);
-            System.arraycopy(ndefUriLen, 0, response, start.size, ndefUriLen.size);
-            System.arraycopy(ndefUriByte, 0, response, start.size + ndefUriLen.size, ndefUriByte.size);
-            System.arraycopy(A_OKAY, 0, response, start.size + ndefUriLen.size + ndefUriByte.size, A_OKAY.size);
-
-            log(ndefUri.toString())
-            log("NDEF_READ_BINARY_GET_NDEF triggered. Our Response: " + Utils.bytesToHex(response))
-
-            isCapableReadContainer = false
-
-            return response
-        }
-
-        log("processCommandApdu() | I don't know what's going on!!!.")
-
-        return "Can I help you?".toByteArray()
 
     }
 
--- app/src/main/java/com/example/nfcpayment/Utils.kt
+++ app/src/main/java/com/example/nfcpayment/Utils.kt
@@ -27,6 +27,22 @@ object Utils {
         }
         return result == 0
     }
+
+    private val HEX_CHARS = "0123456789ABCDEF"
+    fun hexStringToByteArray(data: String) : ByteArray {
+
+        val result = ByteArray(data.length / 2)
+
+        for (i in data.indices step 2) {
+            val firstIndex = HEX_CHARS.indexOf(data[i]);
+            val secondIndex = HEX_CHARS.indexOf(data[i + 1]);
+
+            val octet = firstIndex.shl(4).or(secondIndex)
+            result[i.shr(1)] = octet.toByte()
+        }
+
+        return result
+    }
 }
 
 fun Context.showToast(msg:String) {
